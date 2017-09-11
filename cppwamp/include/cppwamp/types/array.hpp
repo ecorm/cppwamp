3d5e96489d010ee90a979e1e8f65dffc1ad711d2
John <john.ostrowski@butterflyenergy.ca>
2017-09-11 14:14:06 -0300

    Added std::array to/from Varian::Array converter.

---
+++ cppwamp/include/cppwamp/types/array.hpp
@@ -0,0 +1,79 @@
+/*------------------------------------------------------------------------------
+                Copyright Butterfly Energy Systems 2014-2015.
+           Distributed under the Boost Software License, Version 1.0.
+              (See accompanying file LICENSE_1_0.txt or copy at
+                    http://www.boost.org/LICENSE_1_0.txt)
+------------------------------------------------------------------------------*/
+
+#ifndef CPPWAMP_TYPES_ARRAY_HPP
+#define CPPWAMP_TYPES_ARRAY_HPP
+
+//------------------------------------------------------------------------------
+/** @file
+    Provides facilities allowing Variant to interoperate with std::array. */
+//------------------------------------------------------------------------------
+
+#include <array>
+#include "../conversion.hpp"
+#include "../error.hpp"
+
+namespace wamp
+{
+
+//------------------------------------------------------------------------------
+/** Performs the conversion from an array variant to a `std::array`.
+    Users should not use this function directly. Use Variant::to instead. */
+//------------------------------------------------------------------------------
+template <typename T, std::size_t Size>
+void convert(wamp::FromVariantConverter& conv, std::array<T, Size>& array)
+{
+    using namespace wamp;
+    const auto& variant = conv.variant();
+    if (variant.is<Array>() == false)
+    {
+        throw wamp::error::Conversion("Attempting to convert non-array variant "
+                                      "to std::array");
+    }
+
+    std::array<T, Size> newArray;
+    const auto& variantArray = variant.as<Array>();
+    if (variantArray.size() != Size)
+    {
+        throw wamp::error::Conversion("Size mismatched");
+    }
+
+    for (Array::size_type i=0; i<variantArray.size(); ++i)
+    {
+        try
+        {
+            newArray[i] = (variantArray[i].to<T>());
+        }
+        catch (const wamp::error::Conversion& e)
+        {
+            std::string msg = e.what();
+            msg += " (for element #" + std::to_string(i) + ")";
+            throw wamp::error::Conversion(msg);
+        }
+    }
+    array = std::move(newArray);
+}
+
+//------------------------------------------------------------------------------
+/** Performs the conversion from a `std::array` to an array variant.
+    Users should not use this function directly. Use Variant::from instead. */
+//------------------------------------------------------------------------------
+template <typename T, std::size_t Size>
+void convert(wamp::ToVariantConverter& conv, std::array<T, Size>& array)
+{
+    using namespace wamp;
+    Array variantArray;
+    for (const auto& elem: array)
+    {
+        variantArray.emplace_back(Variant::from(elem));
+    }
+    conv.variant() = std::move(variantArray);
+}
+
+} // namespace wamp
+
+#endif // CPPWAMP_TYPES_ARRAY_HPP
