a99d93847b15517786d54f7f3d1a72ea774f4c65
John <john.ostrowski@butterflyenergy.ca>
2018-06-04 11:03:33 -0300

    Forward Variant conversion exceptions as ERROR messages.

--- cppwamp/include/cppwamp/internal/client.hpp
+++ cppwamp/include/cppwamp/internal/client.hpp
@@ -613,13 +613,29 @@ private:
             {
                 Error err = Error("wamp.error.invalid_argument");
                 err.withArgList(Array{e.what()});
-                yield(reqId, std::move(err));
+                if (warningHandler_)
+                {
+                    std::ostringstream oss;
+                    oss << "Received an EVENT with invalid arguments: "
+                        << err.args()
+                        << " (with subId=" << subId
+                        << " pubId=" << pubId << ")";
+                    warn(oss.str());
+                }
             }
             catch (const error::Conversion& e)
             {
                 Error err = Error("wamp.error.invalid_argument");
                 err.withArgList(Array{e.what()});
-                yield(reqId, std::move(err));
+                if (warningHandler_)
+                {
+                    std::ostringstream oss;
+                    oss << "Received an EVENT with invalid arguments: "
+                        << err.args()
+                        << " (with subId=" << subId
+                        << " pubId=" << pubId << ")";
+                    warn(oss.str());
+                }
             }
         });
     }
