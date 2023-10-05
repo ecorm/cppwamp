10a2f37d30c86d0e1bc5f412e553ebe92f028140
ecorm <ecorm@users.noreply.github.com>
2023-10-05 02:45:54 -0300

    HttpJob sketch

--- cppwamp/include/cppwamp/internal/httptransport.hpp
+++ cppwamp/include/cppwamp/internal/httptransport.hpp
@@ -7,6 +7,7 @@
 #ifndef CPPWAMP_INTERNAL_HTTPTRANSPORT_HPP
 #define CPPWAMP_INTERNAL_HTTPTRANSPORT_HPP
 
+#include <boost/asio/steady_timer.hpp>
 #include "../routerlogger.hpp"
 #include "../transports/httpprotocol.hpp"
 #include "websockettransport.hpp"
@@ -16,6 +17,142 @@ namespace wamp
 
 namespace internal
 {
+
+//------------------------------------------------------------------------------
+class HttpJob : public std::enable_shared_from_this<HttpJob>
+{
+public:
+    using Ptr         = std::shared_ptr<HttpJob>;
+    using TcpSocket   = boost::asio::ip::tcp::socket;
+    using Settings    = HttpEndpoint;
+    using SettingsPtr = std::shared_ptr<Settings>;
+    using Handler     = AnyCompletionHandler<void (ErrorOr<AnyHttpAction>)>;
+
+    HttpJob(TcpSocket&& t, SettingsPtr s, const CodecIdSet& c)
+        : tcpSocket_(std::move(t)),
+          timer_(tcpSocket_.get_executor()),
+          codecIds_(c),
+          settings_(std::move(s))
+    {
+        std::string agent = settings_->agent();
+        if (agent.empty())
+            agent = Version::agentString();
+        response_.base().set(boost::beast::http::field::server,
+                             std::move(agent));
+    }
+
+    void process(bool isShedding, Timeout timeout, Handler handler)
+    {
+        isShedding_ = isShedding;
+        handler_ = std::move(handler);
+        auto self = shared_from_this();
+
+        if (timeoutIsDefinite(timeout))
+        {
+            timer_.expires_after(timeout);
+            timer_.async_wait(
+                [this, self](boost::system::error_code ec) {onTimeout(ec);});
+        }
+
+        requestParser_.emplace();
+        // TODO: Set header/body limits
+
+        boost::beast::http::async_read(
+            tcpSocket_, buffer_, *requestParser_,
+            [this, self] (const boost::beast::error_code& netEc, std::size_t)
+            {
+                if (check(netEc))
+                    onRequest();
+            });
+    }
+
+    void cancel() {tcpSocket_.close();}
+
+    const TransportInfo& transportInfo() const {return transportInfo_;}
+
+private:
+    using HttpStatus = boost::beast::http::status;
+    using Parser =
+        boost::beast::http::request_parser<boost::beast::http::string_body>;
+
+    void onTimeout(boost::system::error_code ec)
+    {
+        if (!ec)
+            return fail(TransportErrc::timeout);
+        if (ec != boost::asio::error::operation_aborted)
+            fail(static_cast<std::error_code>(ec));
+    }
+
+    void onRequest()
+    {
+        // Check if we received a websocket upgrade request
+        if (requestParser_->upgrade())
+            return onWebsocketUpgrade();
+
+        // Send an error response if the server connection limit
+        // has been reached
+        if (isShedding_)
+        {
+            return respondThenFail("Connection limit reached",
+                                   HttpStatus::service_unavailable,
+                                   TransportErrc::shedded);
+        }
+
+    }
+
+    void onWebsocketUpgrade()
+    {
+
+    }
+
+    void respondThenFail(std::string msg, HttpStatus status, TransportErrc errc)
+    {
+        namespace http = boost::beast::http;
+        response_.result(status);
+        response_.body() = std::move(msg);
+        auto self = shared_from_this();
+        http::async_write(
+            tcpSocket_, response_,
+            [this, self, errc](boost::beast::error_code netEc, std::size_t)
+            {
+                if (check(netEc))
+                    fail(errc);
+            });
+    }
+
+    bool check(boost::system::error_code netEc)
+    {
+        if (netEc)
+            fail(websocketErrorCodeToStandard(netEc));
+        return !netEc;
+    }
+
+    void fail(std::error_code ec)
+    {
+        if (!handler_)
+            return;
+        handler_(makeUnexpected(ec));
+        handler_ = nullptr;
+        tcpSocket_.close();
+    }
+
+    template <typename TErrc>
+    void fail(TErrc errc)
+    {
+        fail(static_cast<std::error_code>(make_error_code(errc)));
+    }
+
+    TcpSocket tcpSocket_;
+    boost::asio::steady_timer timer_;
+    CodecIdSet codecIds_;
+    TransportInfo transportInfo_;
+    SettingsPtr settings_;
+    Handler handler_;
+    boost::beast::flat_buffer buffer_;
+    boost::optional<Parser> requestParser_;
+    boost::beast::http::response<boost::beast::http::string_body> response_;
+    bool isShedding_ = false;
+};
 
 //------------------------------------------------------------------------------
 class HttpServerTransport : public Transporting
